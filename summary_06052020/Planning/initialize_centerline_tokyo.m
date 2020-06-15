915879b3801cd9d85d0a9f51f662f5dd5e756a39
Ce Hao <57501775+CeHao1@users.noreply.github.com>
2020-06-15 14:27:11 -0700

    adjust kap

--- summary_06052020/Planning/initialize_centerline_tokyo.m
+++ summary_06052020/Planning/initialize_centerline_tokyo.m
@@ -10,7 +10,7 @@ s = data0.s';
 X = data0.X';
 Y = data0.Y';
 psi = data0.psi';
-kap = data0.kap;
+% kap = data0.kap;
 Xl = map_data.Xl';
 Yl = map_data.Yl';
 Xr = map_data.Xr';
@@ -19,28 +19,28 @@ Xc = map_data.Xc';
 Yc = map_data.Yc';
 
 
-figure
-hold on
-plot(Xc,'k')
-plot(Xl)
-plot(Xr)
-plot(X)
-index=(psi>pi/5);
-lp=length(psi);
-index(1:round(lp/2))=0;
-psi(index)=psi(index)-2*pi;
+% figure
+% hold on
+% plot(Xc,'k')
+% plot(Xl)
+% plot(Xr)
+% plot(X)
+% index=(psi>pi/5);
+% lp=length(psi);
+% index(1:round(lp/2))=0;
+% psi(index)=psi(index)-2*pi;
 %%
 trackwidth = params.trackwidth; % a safe gap
 
-wl=ones(1,K)*trackwidth;
-wr=ones(1,K)*trackwidth;
+% wl=ones(1,K)*trackwidth;
+% wr=ones(1,K)*trackwidth;
 
 
 s_intp=intp(s,K);
 X_intp=intp(X,K);
 Y_intp=intp(Y,K);
 psi_intp=intp(psi,K);
-kap_intp=intp(kap,K);
+% kap_intp=intp(kap,K);
 Xl_intp=intp(Xl,K);
 Yl_intp=intp(Yl,K);
 Xr_intp=intp(Xr,K);
@@ -48,11 +48,17 @@ Yr_intp=intp(Yr,K);
 Xc_intp = intp(Xc, K);
 Yc_intp = intp(Yc,K);
 
-% wl = sqrt((Xc_intp - Xl_intp).^2 +(Yc_intp - Yl_intp).^2);
-% wr = sqrt((Xc_intp - Xr_intp).^2 +(Yc_intp - Yr_intp).^2);
+wl = sqrt((Xc_intp - Xl_intp).^2 +(Yc_intp - Yl_intp).^2);
+wr = sqrt((Xc_intp - Xr_intp).^2 +(Yc_intp - Yr_intp).^2);
 
 
 psi_intp = wrapToPi(psi_intp);
+
+diff_psi=wrapToPi(extend(diff(psi)));
+intp_diff_psi=intp(diff_psi,K);
+ds_ori=extend(diff(s_intp));
+kap_intp=intp_diff_psi./ds_ori;
+
 pos=[psi_intp;X_intp;Y_intp;s_intp;kap_intp];
 edge=[Xl_intp;Yl_intp;Xr_intp;Yr_intp;wl;wr];
 
@@ -63,15 +69,14 @@ pos(4,end)=pos(4,end-1)+sqrt((pos(2,end)-pos(2,end-1))^2+(pos(3,end)-pos(3,end-1
 
 % edge=[edge,edge(:,1)];
 
-ds_ori=extend(diff(pos(4,:)));
+
 pos=[pos;ds_ori];
 
-diff_psi=diff(pos(1,:));
-diff_psi=[diff_psi,diff_psi(1)];
-kap=diff_psi./ds_ori;
 
-pos(5,:)=kap;
 
+%%
+
+% pause(0.01)
 
 
 end
